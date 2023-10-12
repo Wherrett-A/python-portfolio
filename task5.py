8f84517fe773f1d8c800f176fb2b82acce223ad8
Wherrett-A <a.wherrett8922@student.leedsbeckett.ac.uk>
2023-10-12 09:21:30 +0100

    comments added to task5

--- task5.py
+++ task5.py
@@ -1,8 +1,12 @@
 if __name__ == "__main__":
+	# takes number of students and desired group size as inputs
 	students = int(input("enter the number of students: "))
 	group_size = int(input("enter the desired group size: "))
 
+	# uses floor division to find the number of whole groups
 	full_groups = students // group_size
+	# used modulus to find the numer of students left over
 	remainder = students % group_size
 
+	# prints the number results
 	print(f"you will have {full_groups} groups and {remainder} people left over")
